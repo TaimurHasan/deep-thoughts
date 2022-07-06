435df9c60f4f55debef4ac8aa07a8fe3b0ffbbc7
TaimurHasan <taimurhasan11@gmail.com>
2022-07-06 18:43:15 -0400

    added mutations

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const { authMiddleware } = require('./utils/auth')
 const express = require('express');
 
 // import ApolloServer
@@ -13,7 +14,8 @@ const PORT = process.env.PORT || 3001;
 // create new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 })
 
 const app = express();
